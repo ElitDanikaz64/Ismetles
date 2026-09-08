0fffc8e8ad10d71dae9e9f495ed28918e9e9e891
Danikaz64 <vitezdani@gmail.com>
2026-09-08 22:36:38 +0200

    Add project files.

---
+++ Model.cs
@@ -0,0 +1,327 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace DELETE
+{
+    public class Plane
+    {
+        public int PlaneId { get; set; }
+        public string PlaneName { get; set; }
+        public int Capacity { get; set; }
+        public int MaxSpeed { get; set; }
+        public int BuiltYear { get; set; }
+        public int TypeId { get; set; }
+
+        public Plane(int planeId, string planeName, int capacity, int maxSpeed, int builtYear, int typeId)
+        {
+            PlaneId = planeId;
+            PlaneName = planeName;
+            Capacity = capacity;
+            MaxSpeed = maxSpeed;
+            BuiltYear = builtYear;
+            TypeId = typeId;
+        }
+
+    }//PlaneId;PlaneName;Capacity;MaxSpeed;BuiltYear;TypeId;TypeName
+
+    public class PlaneType
+    {
+        public int TypeId { get; set; }
+        public string TypeName { get; set; }
+
+        public PlaneType(int typeId, string typeName)
+        {
+            TypeId = typeId;
+            TypeName = typeName;
+        }
+    }
+
+    internal class Model
+    {
+        //II.rész -> Model-View LINQ, filebeolvasás
+
+        // A Program.cs file-ban csak az adatok kiíratását végezd el.
+        // A Model.cs file-ban függvények segítségével készítsd el az alábbi feladatokat.
+
+        // 1. feladat
+        // Készíts két megfelelő adatszerkezetű tárolót public láthatósággal,
+        // amelyekben majd eltárolod az adatokat.
+        // Az egyik adatkonténerbe az alábbi adatok kerüljenek:
+        // PlaneId : int, PlaneName : string, Capacity : int, MaxSpeed : int, BuiltYear : int, TypeId : int
+        // A másik adatkonténerbe az alábbi adatok kerüljenek:
+        // TypeId : int, TypeName : string
+        // Lehetőség szerint készíts osztályokat az összetett adatok eltárolására
+        // (pl. Plane és PlaneType).
+
+        // 2. feladat
+        // Készíts egy private függvényt, amely beolvassa a planes.txt file tartalmát.
+        // A sorok szerkezete:
+        // PlaneId;PlaneName;Capacity;MaxSpeed;BuiltYear;TypeId;TypeName
+        // A megfelelő adatokat mentsd el a megfelelő adatkonténerbe.
+        // Fontos: egy PlaneType csak egyszer szerepeljen a megfelelő adatkonténerben.
+        // Ha például több személyszállító repülőgép található a file-ban,
+        // a "Személyszállító" típus csak egyszer kerüljön bele a típusok listájába.
+
+        Dictionary<PlaneType, List<Plane>> PlanesDict = new();
+
+        private void ReadFile()
+        {
+            StreamReader str = new StreamReader("planes.txt");
+
+            while (!str.EndOfStream)
+            {
+                // 0PlaneId;1PlaneName;2Capacity;3MaxSpeed;4BuiltYear;5TypeId;6TypeName
+                // remeljuk hogy nem változik majd a sorrend:)
+
+                string[] lineContent = str.ReadLine().Split(';');
+
+
+                PlaneType currentPlaneType = new PlaneType(
+
+                    Convert.ToInt32(lineContent[5]), 
+                    lineContent[6]
+
+                );
+
+
+                Plane currentPlane = new Plane(
+
+                    Convert.ToInt32(lineContent[0]),
+                    lineContent[1],
+                    Convert.ToInt32(lineContent[2]),
+                    Convert.ToInt32(lineContent[3]),
+                    Convert.ToInt32(lineContent[4]),
+                    Convert.ToInt32(lineContent[5])
+
+                );
+
+                // nem létezik-e az id?
+                if (!PlanesDict.Where(x=>x.Key.TypeId == currentPlaneType.TypeId).Any())
+                {
+                    List<Plane> newValue = new List<Plane>();
+                    newValue.Add(currentPlane);
+
+                    PlanesDict.Add(currentPlaneType, newValue);
+                }
+                else 
+                {
+                    PlanesDict[currentPlaneType].Add(currentPlane);
+                }
+            }
+
+            str.Close();
+        }
+
+        // 3. feladat
+        // Készíts konstruktort, amelyben meghívod a filebeolvasásra szolgáló függvényt.
+
+        public Model()
+        {
+            ReadFile();
+        }
+
+        // 4. feladat
+        // Készíts függvényt, amely paraméterként kap egy repülőgéptípust.
+        // Add vissza azoknak a repülőgépeknek a neveit, amelyek az adott típushoz tartoznak.
+
+        public List<string> GetPlanesByType(PlaneType planeType)
+        {
+            return PlanesDict[planeType].Select(x=>x.PlaneName).ToList();
+        }
+
+        // 5. feladat
+        // Készíts függvényt, amely paraméterként kap egy évszámot.
+        // Add vissza azoknak a repülőgépeknek a neveit, amelyek a paraméterként megkapott évben már léteztek.
+
+        public List<string> _5(int year)
+        {
+
+            // nem használtuk a SelectMany-t idáig szóval megoldom máshogy is, (amúgy értem mit csinál)
+
+            /*  return PlanesDict.SelectMany(x => x.Value).Where(x => x.BuiltYear <= year).Select(x => x.PlaneName).ToList();  */
+
+
+            List<string> returnList = new();
+
+            foreach (KeyValuePair<PlaneType, List<Plane>> type in PlanesDict)
+            {
+                returnList.AddRange(type.Value.Where(x => x.BuiltYear <= year).Select(x => x.PlaneName));
+            }
+
+            return returnList;
+        }
+
+        // 6. feladat
+        // Készíts statisztikát, amely megmondja, hogy típusonként hány repülőgép tartozik az adott típushoz.
+        // Add vissza egy szótárban a típus nevét és a hozzá tartozó darabszámot.
+
+        public Dictionary<string, int> _6()
+        {
+            return PlanesDict.ToDictionary(x => x.Key.TypeName, x=>x.Value.Count);
+        }
+
+        // 7. feladat
+        // Készíts függvényt, amely megmondja,hogy az egyes típusokhoz tartozó repülőgépek közül mekkora a legnagyobb maximális sebesség.
+        // Add vissza egy szótárban a típus nevét és a hozzá tartozó sebességértéket.
+
+        public Dictionary<string, int> _7()
+        {
+            return PlanesDict.ToDictionary(x => x.Key.TypeName, x => x.Value.Max(x=>x.MaxSpeed));
+        }
+
+        // 8. feladat
+        // Készíts függvényt, amely visszaadja azoknak a repülőgépeknek a neveit ABC-sorrendben, amelyek neve legalább 3 szóból áll.
+
+        public List<string> _8()
+        {
+            return PlanesDict.SelectMany(x=>x.Value).Where(x=>x.PlaneName.Split(' ').Length >= 3).OrderBy(x=>x.PlaneName).Select(x=>x.PlaneName).ToList();
+        }
+
+        // 9. feladat
+        // Készíts függvényt, amely megmondja, hogy típusonként mennyi az átlagos sebesség.
+        // Add vissza egy szótárban a típus nevét és a hozzá tartozó lebegőpontos értéket.
+
+        // 10. feladat
+        // Készíts függvényt, amely paraméterként kap egy minimum kapacitást.
+        // Add vissza azoknak a repülőgépeknek a neveit, amelyek kapacitása legalább akkora, mint a paraméterként kapott érték.
+        // Az eredményt rendezd kapacitás szerint növekvő sorrendbe.
+
+
+        // 11. feladat
+        // Készíts függvényt, amely paraméterként kap egy darabszámot.
+        // Add vissza a megadott darabszámú leggyorsabb repülőgép nevét.
+        // A repülőgépeket MaxSpeed alapján rendezd csökkenő sorrendbe.
+        //
+        // Például:
+        // ha a paraméter értéke 5,
+        // akkor az 5 leggyorsabb repülőgép nevét add vissza.
+
+
+        // 12. feladat
+        // Készíts függvényt, amely paraméterként kap egy minimum és egy maximum kapacitást.
+        // Add vissza azoknak a repülőgépeknek a neveit, amelyek kapacitása a két megadott érték közé esik.
+        // Az eredményt rendezd ABC-sorrendbe.
+
+
+        // 13. feladat
+        // Készíts függvényt, amely paraméterként kap egy évszámot és egy minimum maximális sebességet.
+        // Add vissza azoknak a repülőgépeknek a neveit, amelyek a paraméterként kapott év után készültek, és maximális sebességük legalább akkora, mint a paraméterként kapott sebesség.
+        // Az eredményt MaxSpeed szerint csökkenő sorrendbe rendezd.
+
+
+        // 14. feladat
+        // Készíts függvényt, amely paraméterként kap egy minimum kapacitást.
+        // A legalább ekkora kapacitású repülőgépek közül keresd meg a legnagyobb kapacitásút.
+        // Add vissza a repülőgép nevét.
+
+
+        // 15. feladat
+        // Készíts függvényt, amely paraméterként kap egy repülőgéptípust.
+        // Az adott típushoz tartozó repülőgépek közül keresd meg a legrégebben gyártott repülőgépet.
+        // Add vissza a repülőgép nevét.
+
+
+        // 16. feladat
+        // Készíts függvényt, amely paraméterként kap egy évszámot.
+        // Számítsd ki azoknak a repülőgépeknek az átlagos kapacitását, amelyek a paraméterként kapott évben vagy azután készültek.
+
+
+        // 17. feladat
+        // Készíts függvényt, amely paraméterként kap egy szövegrészletet.
+        // Add vissza azoknak a repülőgépeknek a neveit, amelyek nevében szerepel a paraméterként kapott szöveg.
+        // A keresés során ne számítson,hogy kis- vagy nagybetűkkel adták meg a keresett kifejezést.
+        // Az eredményt ABC-sorrendben add vissza.
+
+
+        // 18. feladat
+        // Készíts függvényt, amely paraméterként kap két szöveget.
+        // A két szöveg egy-egy repülőgépnév kezdete legyen.
+        // Add vissza azokat a repülőgépeket, amelyek neve az egyik vagy a másik paraméterként kapott szöveggel kezdődik.
+        // Például:
+        // "Airbus"
+        // "Boeing"
+        //
+        //Rendezd őket gyártási év szerint csökkenő sorrendbe.
+
+
+
+        // 19. feladat
+        // Készíts függvényt, amely paraméterként kap egy repülőgéptípust és egy minimum sebességet.
+        // Add vissza az adott típushoz tartozó olyan repülőgépek neveit, amelyek maximális sebessége nagyobb, mint a paraméterként kapott minimum sebesség.
+        // Az eredményt MaxSpeed szerint csökkenő sorrendbe rendezd.
+
+        // 20. feladat
+        // Készíts függvényt, amely típusonként kiszámítja az összes férőhely számát.
+        // Add vissza egy szótárban:
+        // TypeName -> összes Capacity
+        // Például:
+        // "Személyszállító" -> 1250
+
+
+        // 21. feladat
+        // Készíts függvényt, amely típusonként megkeresi a legnagyobb kapacitású repülőgépet.
+        // Add vissza minden típushoz a legnagyobb kapacitású repülőgép nevét.
+        //
+        // A feladat megoldásánál használj GroupBy-t,
+
+
+        // 22. feladat
+        // Készíts függvényt, amely visszaadja a 3 legújabb olyan repülőgépet, amely legalább 150 fő befogadására képes.
+
+        // 23. feladat
+        // Készíts függvényt, amely paraméterként kap egy évszámot.
+        // Számold meg típusonként, hogy hány olyan repülőgép van, amely a megadott év után készült.
+        // Add vissza egy szótárban:
+        // TypeName -> darabszám.
+
+
+        // 24. feladat
+        // Készíts függvényt, amely megkeresi azokat a repülőgéptípusokat,  amelyekhez tartozó repülőgépek átlagos maximális sebessége  nagyobb 700 km/h-nál.
+        // Add vissza csak a típusok neveit, az átlagos sebességük szerint csökkenő sorrendben.
+
+
+        // 25. feladat
+        // Készíts függvényt, amely visszaadja az első 3 olyan repülőgépet,
+        // amelyek:
+        //
+        // - legalább 200 fő kapacitásúak,
+        // - 1990 után készültek,
+        // - maximális sebességük legalább 800 km/h.
+
+
+
+        // 26. feladat
+        // Készíts függvényt, amely minden repülőgépből egy szöveges adatot készít
+        // Select használatával.
+        //
+        // A visszaadott lista elemei ilyen formájúak legyenek:
+        //
+        // "Airbus A350 - 350 fő - 945 km/h"
+        //
+        // A listát a repülőgépek neve szerint ABC-sorrendben add vissza.
+
+
+        // 27. feladat
+        // Készíts függvényt, amely megvizsgálja, hogy van-e olyan repülőgép az adatok között, amely:
+        // - 2015 után készült,
+        // - legalább 300 fő kapacitású,
+        // - és legalább 900 km/h maximális sebességű.
+        //
+        // A függvény bool típusú értékkel térjen vissza.
+
+
+        // 28. feladat
+        // Készíts függvényt, amely típusonként visszaadja a három leggyorsabb repülőgépet.
+        // (Group by)
+
+
+        // 29. feladat
+        // Készíts függvényt, amely meghatározza, melyik repülőgéptípus rendelkezik a legnagyobb összesített kapacitással.
+        // (Group by)
+
+        // 30. feladat
+        // Minden függvényedet hívd meg a Program.cs file-ban,
+        // és írd ki az eredményeket.
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,431 @@
+﻿//12 szoftver szabadság alatt gyakorlás :
+
+//I.rész -> Bevezetés
+
+// Kérj be a felhasználótól tetszőleges mennyiségű egész számot egyetlen sorban, vesszővel elválasztva.
+// Példa:
+// 12,45,7,31,90,22,18,67
+
+// A bekért szöveget bontsd szét vessző mentén.
+
+// A kapott értékeket alakítsd át egész számokká, majd tárold el őket egy listában. (.ToList)
+
+using DELETE;
+
+List<int> szamok = new();
+Console.ReadLine().Split(',').ToList().ForEach(x => szamok.Add(Convert.ToInt32(x)));
+
+
+// 1. feladat
+// Adja vissza a listában lévő legnagyobb számot
+
+int GetMax()
+{
+    int max = szamok[0];
+
+    foreach (int x in szamok)
+        if (x > max)
+            max = x;
+
+    return max;
+
+    /*
+     vagy csak
+    return szamok.Max();
+     
+     */
+}
+
+// 2. feladat
+// Számítsa ki és adja vissza a listában található számok átlagát.
+// Figyelj arra, hogy az eredmény lebegőpontos szám legyen.
+
+double GetAvg()
+{
+    return szamok.Average();
+}
+
+// 3. feladat
+// Számolja meg és adja vissza,hogy hány darab 30-nál nagyobb szám található a listában.
+
+int CountAboveThirty()
+{
+    int counter = 0;
+
+    foreach (int x in szamok)
+        if (x > 30)
+            counter++;
+
+    return counter;
+}
+
+// A kiírás például ilyen formában történjen:
+//
+// Legnagyobb szám: 90
+// A számok átlaga: 36,5
+// 30-nál nagyobb számok száma: 4
+
+Console.WriteLine($"Legnagyobb szám: {GetMax()}\nA számok átlaga: {GetAvg()}\n30-nál nagyobb számok száma: {CountAboveThirty()}");
+
+// 4.feladat
+// Adja vissza egy új listában az összes negatív számot.
+
+List<int> GetNegatives()
+{
+    List<int> list = new();
+
+    foreach (int x in szamok)
+        if (x < 0)
+            list.Add(x);
+
+    return list;
+}
+
+// 5.feladat
+// Add vissza a listában lévő számok közül a három legnagyobbat (Linq-t már ajánlott használni)
+// Ha a listában háromnál kevesebb szám található,akkor az összes rendelkezésre álló számot adja vissza.
+
+List<int> _5()
+{
+    if (szamok.Count <= 3) return szamok;
+
+    return szamok.OrderByDescending(x=>x).Take(3).ToList();
+}
+
+
+// ---------------- kiírás innen ---------------- //
+
+Model MODEL = new Model();
+
+
+
+//III.rész -> Öröklődés:
+
+// Készítsd el az alábbi osztályhierarchiát!
+// Minden osztály külön file-ban legyen.
+
+// Az osztályok:
+// Vehicle
+//   |
+//   +-- AirVehicle
+//          |
+//          +-- Airplane
+//          |      |
+//          |      +-- PassengerPlane
+//          |
+//          +-- Helicopter
+//
+// A feladat során használd az öröklődést, private és protected adattagokat,
+// konstruktorokat, függvényeket, valamint virtual és override függvényeket.
+
+
+// 1. osztály – Vehicle
+
+// Készíts egy Vehicle nevű osztályt.
+
+// Protected adattag:
+// - name : string
+
+// Private adattag:
+// - builtYear : int
+
+// Készíts konstruktort.
+// Paraméterei:
+// - name : string
+// - builtYear : int
+// Feladata: állítsa be az osztály adattagjainak kezdőértékét.
+
+
+// Készíts egy GetBuiltYear nevű függvényt.
+// Paraméter: nincs
+// Visszatérési típus: int
+// Feladata: adja vissza a jármű gyártási évét.
+
+
+// Készíts egy GetAge nevű függvényt.
+// Paraméter:
+// - currentYear : int
+// Visszatérési típus: int
+// Feladata: számítsa ki, hogy hány éves a jármű.
+// Például: gyártási év: 2015, aktuális év: 2026, eredmény: 11
+
+
+// Készíts egy virtual GetVehicleInfo nevű függvényt.
+// Paraméter: nincs
+// Visszatérési típus: string
+// Feladata: adjon vissza egy szöveget a jármű nevéről és gyártási évéről.
+
+
+// 2. osztály – AirVehicle
+
+// Készíts egy AirVehicle nevű osztályt, amely a Vehicle osztályból öröklődik.
+
+// Protected adattag:
+// - maxSpeed : int
+
+// Private adattag:
+// - maxAltitude : int
+
+// Készíts konstruktort.
+// Paraméterei:
+// - name : string
+// - builtYear : int
+// - maxSpeed : int
+// - maxAltitude : int
+// A konstruktorban az ősosztály konstruktorát is használd.
+
+
+// Készíts egy GetMaxAltitude nevű függvényt.
+// Paraméter: nincs
+// Visszatérési típus: int
+// Feladata: adja vissza a maximális repülési magasságot.
+
+
+// Készíts egy IsFasterThan nevű függvényt.
+// Paraméter:
+// - speed : int
+// Visszatérési típus: bool
+// Feladata: adjon vissza true értéket, ha a légi jármű maximális sebessége
+// nagyobb a paraméterként kapott sebességnél.
+// Egyébként false értéket adjon vissza.
+
+
+// 3. osztály – Airplane
+
+// Készíts egy Airplane nevű osztályt, amely az AirVehicle osztályból öröklődik.
+
+// Protected adattag:
+// - capacity : int
+
+// Private adattag:
+// - fuelAmount : int
+
+// Készíts konstruktort.
+// Paraméterei:
+// - name : string
+// - builtYear : int
+// - maxSpeed : int
+// - maxAltitude : int
+// - capacity : int
+// - fuelAmount : int
+
+// Használd az ősosztály konstruktorát.
+
+
+// Készíts egy GetFuelAmount nevű függvényt.
+// Paraméter: nincs
+// Visszatérési típus: int
+// Feladata: adja vissza az aktuális üzemanyag-mennyiséget.
+
+
+// Készíts egy Refuel nevű függvényt.
+// Paraméter:
+// - amount : int
+// Visszatérési típus: void
+// Feladata: növelje az üzemanyag mennyiségét a paraméterként kapott értékkel.
+// Negatív értéket ne lehessen hozzáadni.
+
+
+// Készíts egy ConsumeFuel nevű függvényt.
+// Paraméter:
+// - amount : int
+// Visszatérési típus: bool
+// Feladata: ha van legalább annyi üzemanyag, mint a paraméterként kapott érték,
+// csökkentse vele az üzemanyag mennyiségét és adjon vissza true értéket.
+// Ha nincs elegendő üzemanyag, ne változtasson semmit és adjon vissza false értéket.
+
+
+// Készíts egy HasLargeCapacity nevű függvényt.
+// Paraméter:
+// - limit : int
+// Visszatérési típus: bool
+// Feladata: adjon vissza true értéket, ha a repülőgép kapacitása
+// legalább akkora, mint a paraméterként kapott határérték.
+
+
+// 4. osztály – PassengerPlane
+
+// Készíts egy PassengerPlane nevű osztályt, amely az Airplane osztályból öröklődik.
+
+// Private adattag:
+// - businessSeats : int
+
+// Private adattag:
+// - ticketPrice : int
+
+// Készíts konstruktort.
+// Paraméterei:
+// - name : string
+// - builtYear : int
+// - maxSpeed : int
+// - maxAltitude : int
+// - capacity : int
+// - fuelAmount : int
+// - businessSeats : int
+// - ticketPrice : int
+
+
+// Készíts egy GetTicketPrice nevű függvényt.
+// Paraméter: nincs
+// Visszatérési típus: int
+// Feladata: adja vissza egy normál jegy árát.
+
+
+// Készíts egy CalculateRevenue nevű függvényt.
+// Paraméter:
+// - soldTickets : int
+// Visszatérési típus: int
+// Feladata: számítsa ki, hogy mennyi bevétel keletkezett,
+// ha a paraméterként megadott számú jegyet eladták.
+// Feltételezheted, hogy minden jegy azonos árú.
+
+
+// Készíts egy GetBusinessSeatPercentage nevű függvényt.
+// Paraméter: nincs
+// Visszatérési típus: double
+// Feladata:
+// számítsa ki, hogy a teljes férőhely hány százaléka business osztály.
+// Például:
+// capacity = 200
+// businessSeats = 20
+// elvart_eredmeny = 10.0
+
+
+// 5. osztály – Helicopter
+
+// Készíts egy Helicopter nevű osztályt, amely az AirVehicle osztályból öröklődik.
+
+// Private adattag:
+// - rotorCount : int
+
+// Private adattag:
+// - isRescueHelicopter : bool
+
+// Készíts konstruktort.
+// Paraméterei:
+// - name : string
+// - builtYear : int
+// - maxSpeed : int
+// - maxAltitude : int
+// - rotorCount : int
+// - isRescueHelicopter : bool
+
+
+// Készíts egy IsSuitableForRescue nevű függvényt.
+// Paraméter: nincs
+// Visszatérési típus: bool
+// Feladata: adja vissza az isRescueHelicopter adattag értékét.
+
+
+// Készíts egy GetRotorInfo nevű függvényt.
+// Paraméter: nincs
+// Visszatérési típus: string
+// Feladata: adjon vissza egy szöveget a rotorok számáról.
+// Például: "A helikopter 2 rotorral rendelkezik."
+
+
+// 6. feladat – override
+
+// Írd felül a PassengerPlane és a Helicopter osztályban
+// a Vehicle osztályból örökölt GetVehicleInfo() függvényt.
+
+
+// A PassengerPlane GetVehicleInfo() függvénye:
+// Paraméter: nincs
+// Visszatérési típus: string
+// Feladata: adja vissza legalább az alábbi adatokat:
+// - name
+// - builtYear
+// - maxSpeed
+// - capacity
+// - ticketPrice
+
+
+// A Helicopter GetVehicleInfo() függvénye:
+// Paraméter: nincs
+// Visszatérési típus: string
+// Feladata: adja vissza legalább az alábbi adatokat:
+// - name
+// - builtYear
+// - maxSpeed
+// - rotorCount
+// - isRescueHelicopter
+
+
+// 7. feladat – Példányosítás
+
+// A Program.cs file-ban hozz létre legalább:
+// - 2 darab PassengerPlane objektumot
+// - 2 darab Helicopter objektumot
+
+
+// Példa adatok:
+
+// PassengerPlane:
+// name: "Airbus A350"
+// builtYear: 2013
+// maxSpeed: 945
+// maxAltitude: 13100
+// capacity: 350
+// fuelAmount: 120000
+// businessSeats: 40
+// ticketPrice: 150000
+
+
+// PassengerPlane:
+// name: "Boeing 787 Dreamliner"
+// builtYear: 2009
+// maxSpeed: 954
+// maxAltitude: 13100
+// capacity: 330
+// fuelAmount: 110000
+// businessSeats: 36
+// ticketPrice: 170000
+
+
+// Helicopter:
+// name: "Airbus H145"
+// builtYear: 2014
+// maxSpeed: 268
+// maxAltitude: 5200
+// rotorCount: 2
+// isRescueHelicopter: true
+
+
+// Helicopter:
+// name: "Robinson R44"
+// builtYear: 1992
+// maxSpeed: 240
+// maxAltitude: 4300
+// rotorCount: 2
+// isRescueHelicopter: false
+
+
+// 8. feladat – Függvények tesztelése
+
+// Hívd meg az elkészített függvényeket a Program.cs file-ban.
+
+// Írasd ki:
+// - az összes jármű korát
+// - melyik légi jármű gyorsabb 500 km/h-nál
+// - az utasszállítók jegyárát
+// - az utasszállítók business férőhelyeinek százalékát
+// - a helikopterek rotorinformációját
+// - hogy melyik helikopter alkalmas mentésre
+
+
+// Tankolj 10000 egység üzemanyagot az egyik utasszállítóba.
+// Írasd ki az új üzemanyag-mennyiséget.
+
+
+// Fogyassz el 50000 egység üzemanyagot.
+// Írasd ki, hogy sikeres volt-e a művelet,
+// majd írasd ki az új üzemanyag-mennyiséget.
+
+
+// Számítsd ki, hogy az első utasszállító mekkora bevételt termel,
+// ha 280 jegyet értékesítettek.
+
+
+// Végül hívd meg mindegyik objektum GetVehicleInfo() függvényét.
+// Figyeld meg, hogy a PassengerPlane és a Helicopter objektumoknál az override-olt változat fut le.
